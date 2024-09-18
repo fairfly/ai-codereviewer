399aa733eca048534338f0eb9d3997b106626df0
amigold <ami.gold@gmail.com>
2024-09-18 22:11:15 +0300

    1

---
+++ dist/index.mjs
@@ -0,0 +1,7 @@
+/**
+ * The entrypoint for the action.
+ */
+import { main } from './main.mjs';
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main();
+//# sourceMappingURL=index.mjs.map
