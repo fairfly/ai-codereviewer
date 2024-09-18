817c518447362f689f4d16cd2842e8c99cdb0ad4
amigold <ami.gold@gmail.com>
2024-09-18 22:28:27 +0300

    works?

--- dist/main.mjs
+++ dist/main.mjs
@@ -69,6 +69,7 @@ function createPrompt(file, chunk, prDetails) {
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
+- Do not suggest removing a trailing comma
 - IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
